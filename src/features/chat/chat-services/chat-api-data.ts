1b0716c939fdc9fcbc79ac956a036ed097e0b934
Rasmus Standar <rasmus.standar@gmail.com>
2023-11-06 10:01:22 +0100

    Update chat-api-data.ts

--- src/features/chat/chat-services/chat-api-data.ts
+++ src/features/chat/chat-services/chat-api-data.ts
@@ -113,7 +113,7 @@ export const ChatAPIData = async (props: PromptGPTProps) => {
 
 const findRelevantDocuments = async (query: string, chatThreadId: string) => {
   const relevantDocuments = await similaritySearchVectorWithScore(query, 10, {
-    filter: `user eq '${await userHashedId()}' and chatThreadId eq '${chatThreadId}'`,
+    /*filter: `user eq '${await userHashedId()}' and chatThreadId eq '${chatThreadId}'`,*/
   });
 
   return relevantDocuments;
